6b1dc0cbe577252cb86df6f14f1e6ffc76c8d8a4
Will Bettridge-Radford <william.bettridge.radford@gmail.com>
2023-04-07 23:29:27 +0100

    use re for fuzzy matching

--- pymenu/__main__.py
+++ pymenu/__main__.py
@@ -1,3 +1,5 @@
+import re
+
 import tkinter as tk
 from tkinter.ttk import *
 
@@ -9,25 +11,106 @@ date
 elderberry
 """.split()
 
-
 def update_listbox(event):
-    # Clear the Listbox
-    listbox.delete(0, tk.END)
+    if not event is None:
+        if event.state:
+            return
+        if event.keysym in ['Up', 'Down', 'Control_L']:
+            return
 
-    # Get the current text in the Entry widget
     text = entry.get()
 
-    # Loop through the dummy data and add matching items to the Listbox
-    for item in input_list:
-        if text.lower() in item.lower():
+    if text == '':
+        listbox.delete(0, tk.END)
+        for item in input_list:
             listbox.insert(tk.END, item)
-            
-    # select the first item
+        listbox.select_set(0)
+        return
+
+
+    xr = eval_expr(text)
+    results = filter(input_list, text)
+
+    if not xr is None or results:
+        listbox.delete(0, tk.END)
+
+    if not xr is None:
+        listbox.insert(tk.END, xr)
+
+    if results:
+        for item in results:
+            listbox.insert(tk.END, item)
+
     listbox.select_set(0)
+
+
+def filter(input_list, text):
+    results = []
+
+    needle = '.*'.join(text)
+
+    try:
+        pattern = re.compile(needle, re.IGNORECASE)
+    except:
+        return []
+
+    for item in input_list:
+        if pattern.search(item):
+            results.append(item)
+
+    return results
+
+
+def eval_expr(x):
+    try:
+        result = eval(x)
+    except:
+        result = None
+
+    return result
+
+
+def handle_return(event):
+    index = listbox.curselection()
+    value = listbox.get(index)
+    print(value)
+    root.destroy()
+
+
+def select_next(event):
+    current = listbox.curselection()
+    if current:
+        next = current[-1] + 1
+        if next >= listbox.size():
+            next = listbox.size() - 1
+        listbox.selection_clear(0, tk.END)
+        listbox.selection_set(next)
+        listbox.activate(next)
+
+
+def select_previous(event):
+    current = listbox.curselection()
+    if current:
+        previous = current[0] - 1
+        if previous < 0:
+            previous = 0
+        listbox.selection_clear(0, tk.END)
+        listbox.selection_set(previous)
+        listbox.activate(previous)
+
+
+def delete_line(event):
+    entry.delete(0, tk.END)
+
+
+def handle_click(event):
+    index = listbox.nearest(event.y)
+    item = listbox.get(index)
+    print('click', item)
 
 root = tk.Tk()
 
-font = ('Arial', 32)
+font = ('Arial', 50)
 
 # Create an Entry widget for user input
 entry = tk.Entry(root, font=font)
@@ -41,35 +124,35 @@ listbox.pack()
 entry.bind('<KeyRelease>', update_listbox)
 entry.focus_set()
 
-# Add some dummy data to the Listbox
-for elem in input_list:
-    listbox.insert(tk.END, elem)
-
-listbox.select_set(0)
+update_listbox(None)
 
 # override_redirect=True makes the window borderless
 root.overrideredirect(True)
 
-# Get the requested size of the window
-req_width = root.winfo_reqwidth()
-req_height = root.winfo_reqheight()
+root.update()
 
-# Get the screen dimensions
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
-# Calculate the coordinates of the top-left corner of the window
-x = (screen_width - req_width) // 2
-y = (screen_height - req_height) // 2
+window_width = root.winfo_reqwidth()
+window_height = root.winfo_reqheight()
 
-# Move the window to the center of the screen
-root.geometry(f"+{x}+{y}")
+x = (screen_width // 2) - (window_width // 2)
+y = (screen_height // 2) - (window_height // 2)
 
-root.tk.call('tk', 'scaling', 2.0)
+root.geometry("+{}+{}".format(x, y))
 
 root.bind('<Escape>', lambda e: root.destroy())
-root.bind('<Return>', lambda e: root.destroy())
+root.bind('<Return>', handle_return)
+root.bind('<FocusOut>', lambda e: root.destroy())
+root.bind('<Up>', select_previous)
+root.bind('<Down>', select_next)
+root.bind('<Control-p>', select_previous)
+root.bind('<Control-n>', select_next)
+root.bind('<Control-u>', delete_line)
 
+listbox.bind('<Button-1>', handle_click)
 root.protocol("WM_DELETE_WINDOW", root.destroy)
 
 root.mainloop()
+
